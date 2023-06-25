f2bf6053784b2b7af2f44354ad206dfdebd6daa9
Dendi Handian <contact.dendi@yahoo.com>
2023-06-26 04:58:30 +0700

    skena ingestion tasks

--- docker/airflow/dags/prod/skena_analysis/skena_tasks.py
+++ docker/airflow/dags/prod/skena_analysis/skena_tasks.py
@@ -1,7 +1,31 @@
+from _utils.mongo.client import mongo
 from _utils.spotify.client import search_for_playlists
 from airflow.decorators import task
+from pymongo import UpdateOne
+from pymongo.errors import BulkWriteError
+import pprint
 
 @task
 def search_and_store_playlist(q):
-    playlist = search_for_playlists(q)
-    print('playlist_len', len(playlist))
+
+    skena_analysis = mongo['skena_analysis']
+    raw_playlists = skena_analysis["raw_playlists"]
+
+    print('FYI skena_analysis.list_collection_names', skena_analysis.list_collection_names())
+
+    playlists = search_for_playlists(q)
+    print('FYI len(playlists)', len(playlists))
+
+    for index, playlist in enumerate(playlists):
+        playlists[index]['_id'] = playlist['id']
+
+    upsert_requests = [UpdateOne({'_id': playlist['_id']}, {'$set':playlist}, upsert=True) for playlist in playlists]
+
+    try:
+
+        result = raw_playlists.bulk_write(upsert_requests)
+        print(f"FYI Upserted documents: {result.upserted_count}")
+        print(f"FYI Modified documents: {result.modified_count}")
+
+    except BulkWriteError as bwe:
+        pprint(bwe.details)
