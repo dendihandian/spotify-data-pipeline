ddba54a6948935059075ba54fddf314e53474409
Dendi Handian <contact.dendi@yahoo.com>
2023-06-29 14:29:59 +0700

    refactor spotify client and skena tasks

--- docker/airflow/dags/_utils/spotify/client.py
+++ docker/airflow/dags/_utils/spotify/client.py
@@ -1,11 +1,22 @@
 import requests
 from _utils.redis.client import redis
 from _configs.spotify import SPOTIFY_CONFIG
+import json
+import uuid
+from time import sleep
 
-sporify_api_base_url        = 'https://api.spotify.com/v1'
+spotify_api_base_url        = 'https://api.spotify.com/v1'
 rediskey_spotify_credential = 'spotify:credentials'
 
+plurals = {
+    'playlist'  : 'playlists',
+    'track'     : 'tracks',
+}
+
 #### LOW-LEVEL FUNCTIONS
+
+def getter(url_path, token, params):
+    return requests.get(spotify_api_base_url + url_path, headers={'Authorization' : f'Bearer {token}'}, params=params)
 
 def request_access_token():
     response = requests.post(
@@ -47,46 +58,68 @@ def get_access_token():
 
     return redis.hget(rediskey_spotify_credential, 'access_token')
 
-def search_for_item(q, type=['playlist'], market=['ID'], limit=50, offset=0):
+### API FUNCTIONS
 
-    token = get_access_token()
+def search_for_item(q, search_type='playlist', market=['ID'], delay=0):
 
-    response = requests.get(
-        url=f"{sporify_api_base_url}/search",
-        headers={
-            'Authorization' : f'Bearer {token}',
-        },
-        params={
-            'q'         : q,
-            'type'      : ",".join(type),
-            'market'    : ",".join(market),
-            'limit'     : limit,
-            'offset'    : offset,
-        }
-    )
+    _search_type    = search_type
+    _market         = ",".join(list(set(sorted(market))))
+    cache_hash      = str(uuid.uuid5(uuid.NAMESPACE_DNS, q+_search_type+_market))
+    cache_key       = f'spotify:api-cache:search_for_item:{cache_hash}'
 
-    # print('response.status_code:', response.status_code)
-    # print('response.status_code:', response.text)
+    if redis.exists(cache_key):
+        return json.loads(redis.get(cache_key))
 
-    return response.json()
-
-#### HIGH-LEVEL FUNCTIONS
-
-def search_for_playlists(q, market=['ID']):
+    token   = get_access_token()
+    items  = []
 
     limit           = 50
     offset          = 0
     offset_exceed   = False
-    playlists       = []
     total           = None
 
     while not offset_exceed:
+        response        = getter(f"/search", token, params={'q':q, 'type':_search_type, 'market':_market, 'limit':limit, 'offset':offset})
+        response_json   = response.json()
 
-        response        = search_for_item(q, ['playlist'], market, limit=limit, offset=offset)
-        total           = int(response['playlists']['total'])
-
-        playlists       = playlists + response['playlists']['items']
+        total           = int(response_json[plurals[search_type]]['total'])
+        items           = items + response_json[plurals[search_type]]['items']
         offset          = offset + limit
         offset_exceed   = True if (offset >= total) else False
 
-    return playlists
+    redis.set(cache_key, json.dumps(items), 3600)
+    sleep(delay)
+
+    return items
+
+def get_playlist_tracks(playlist_id, market=['ID'], fields='', delay=0):
+
+    _market     = "-".join(list(set(sorted(market))))
+    _fields     = fields
+    cache_hash  = str(uuid.uuid5(uuid.NAMESPACE_DNS, playlist_id+_market+_fields))
+    cache_key   = f'spotify:api-cache:get_playlist_tracks:{cache_hash}'
+
+    if redis.exists(cache_key):
+        return json.loads(redis.get(cache_key))
+
+    token   = get_access_token()
+    tracks  = []
+
+    limit           = 50
+    offset          = 0
+    offset_exceed   = False
+    total           = None
+
+    while not offset_exceed:
+        response        = getter(f"/playlists/{playlist_id}/tracks", token, params={'market':",".join(market), 'fields':fields, 'limit':limit, 'offset':offset})
+        response_json   = response.json()
+
+        total           = int(response_json['total'])
+        tracks          = tracks + response_json['items']
+        offset          = offset + limit
+        offset_exceed   = True if (offset >= total) else False
+
+    redis.set(cache_key, json.dumps(tracks), 86400)
+    sleep(delay)
+
+    return tracks
--- docker/airflow/dags/prod/skena_analysis/skena_tasks.py
+++ docker/airflow/dags/prod/skena_analysis/skena_tasks.py
@@ -1,9 +1,10 @@
 from _utils.mongo.client import mongo
-from _utils.spotify.client import search_for_playlists
+from _utils.spotify.client import search_for_item, get_playlist_tracks
 from airflow.decorators import task
 from pymongo import UpdateOne
 from pymongo.errors import BulkWriteError
 import pprint
+import time
 
 @task
 def ingest_playlists(q):
@@ -11,7 +12,7 @@ def ingest_playlists(q):
     skena_analysis = mongo['skena_analysis']
     raw_playlists = skena_analysis["raw_playlists"]
 
-    playlists = search_for_playlists(q)
+    playlists = search_for_item(q, search_type='playlist', market=['ID'])
     print('FYI len(playlists)', len(playlists))
 
     for index, playlist in enumerate(playlists):
@@ -34,6 +35,25 @@ def ingest_playlists(q):
 @task
 def get_tracks_from_playlists(playlist_ids):
 
+    playlists_tracks = {}
+
     if len(playlist_ids) > 0:
 
-        print('FYI playlist_ids', playlist_ids)
+        for playlist_id in playlist_ids:
+            tracks = get_playlist_tracks(playlist_id, market=['ID'], delay=5)
+            playlists_tracks[playlist_id] = tracks
+
+        if len(playlists_tracks.keys()) > 0:
+            raw_playlists_tracks = mongo['skena_analysis']['raw_playlists_tracks']
+            upsert_requests = [UpdateOne({'_id': playlist_id['_id']}, {'$set': playlists_tracks[playlist_id]}, upsert=True) for playlist_id in playlists_tracks.keys()]
+
+            try:
+
+                result = raw_playlists_tracks.bulk_write(upsert_requests)
+                print(f"FYI Upserted documents: {result.upserted_count}")
+                print(f"FYI Modified documents: {result.modified_count}")
+
+            except BulkWriteError as bwe:
+                pprint(bwe.details)
+
+    return playlists_tracks
